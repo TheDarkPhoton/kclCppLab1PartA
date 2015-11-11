ae1b7388047431cda4400b9e515728c154289708
Dovydas Rupsys <dovydas.rupsys@kcl.ac.uk>
2015-11-11 08:58:29 +0000

    Talkamoton 800
    An example of simple communication simulator.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,28 @@
 using namespace std;
 
 int main() {
-    cout << "Hello World" << endl;
+    string input;
+
+    cout << "\nHello I'm Talkamoton 800 the friendly robot you can talk to :)\n";
+    cout << "What is your name?\n";
+
+    getline(cin, input);
+
+    cout << "hello " << input << " how's the c++ workshop so far?\n";
+
+    getline(cin, input);
+    while (input != "good" && input != "bad"){
+        cout << "I don't understand that :(" << endl;
+        cout << "Enter 'good' or 'bad'" << endl;
+        getline(cin, input);
+    }
+
+    if (input == "good"){
+        cout << "I'm glad to hear =]";
+    }
+    else if (input == "bad") {
+        cout << "Get out of here ;D";
+    }
 
     return 0;
 }
