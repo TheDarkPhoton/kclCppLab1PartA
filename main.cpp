93834febe26355c522639e8fd7a066c3959d7c24
Dovydas Rupsys <dovydas.rupsys@kcl.ac.uk>
2015-11-11 17:23:15 +0000

    Some final touches to the example

--- main.cpp
+++ main.cpp
@@ -14,9 +14,17 @@ string processInput(string command){
     return command.substr(1, command.size() - 1);
 }
 
+void displayHelp(){
+    cout << "To check if some word is a palindrome word enter ? followed by the word to be checked.\n";
+    cout << "e.g. ?house or ? house, ?deleveled, ...\n";
+    cout << "Note that the first character must be a question mark.\n\n";
+
+    cout << "Alternatively enter 'exit' to quit the app:\n";
+}
+
 int main() {
-    cout << "Welcome to Palindrome Checker" << endl;
-    cout << "Type 'help' to see available commands." << endl;
+    cout << "Welcome to Palindrome Checker\n";
+    cout << "Type 'help' to see available commands.\n";
 
     string command;
     while (true){
@@ -26,28 +34,27 @@ int main() {
         getline(cin, command);
 
         //if the first character is the question mark then do
-        if (command.c_str()[0] == '?'){
+        if (command.substr(0, 1) == "?"){
             string word = processInput(command);
 
             if (isPalindrome(word))
-                cout << word << " is a palindrome" << endl;
+                cout << word << " is a palindrome\n";
             else
-                cout << word << " is not a palindrome" << endl;
+                cout << word << " is not a palindrome\n";
         }
         //if help was entered display help messages
         else if (command == "help"){
-            cout << "To check if some word is a palindrome word enter ? followed by the word to be checked." << endl;
-            cout << "e.g. ?house or ? house, ?deleveled, ..." << endl;
-            cout << "Note that the first character must be a question mark." << endl << endl;
-
-            cout << "Alternatively enter 'exit' to quit the app:" << endl;
+            displayHelp();
         }
         // if exit was entered brake/quit the loop
         else if (command == "exit"){
             break;
         }
+        else{
+            cout << "Command '" << command << "' not recognized\n";
+        }
 
-        cout << endl << "Enter a new command" << endl;
+        cout << endl << "Enter a new command\n";
     }
 
     return 0;
