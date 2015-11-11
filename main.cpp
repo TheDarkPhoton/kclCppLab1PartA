e065eff172c65784cb575341b180f702f1a254c9
Kyle Bagshaw <rkbagshaw10@gmail.com>
2015-11-11 23:33:48 +0000

    add example showcasing basic primitive types

--- main.cpp
+++ main.cpp
@@ -2,23 +2,27 @@
 
 using namespace std;
 
-int add(int* x, int* y)
-{
-	int sum = *x + *y;
-	return sum;
-}
-
 int main()
 {
-	int x = 1;
-	int* x_ptr = &x;
+	/* integer types - whole numbers */
 
-	int y = 2;
-	int* y_ptr = &y;
+	/* each of these types can ascendingly represent a greater range of
+	   values but they consume more memory */
+	char a = 127;
+	short b = 32767;
+	int c = 2147483647;
+	long d = 4294967295;
 
-	int z = add(x_ptr, y_ptr);
+	/* each of the above types dedicate half the memory they consume to
+	   represent negative numbers but this type does not so it can
+	   represent twice as many positive numbers */
+	unsigned char e = 255;
 
-	cout << z << endl;
+	/* floating point types - real numbers */
+	float x = 1.01234;
+	/* a double can represent a real number more precisely than a float
+	   but again consumes more memory */
+	double y = 1.0123456789;
 
 	return 0;
 }
