c6dada6fb1f7da787a4c8c5cb2933a926378a8e7
Dovydas Rupsys <dovydas.rupsys@kcl.ac.uk>
2015-11-11 20:04:24 +0000

    Simple Array loop example

--- main.cpp
+++ main.cpp
@@ -5,5 +5,17 @@ using namespace std;
 int main() {
     cout << "Hello World" << endl;
 
+    string names[] = {
+            "January", "February",
+            "March", "April", "May",
+            "June", "July", "August",
+            "September", "October", "November",
+            "December",
+    };
+
+    for (int i = 0; i < end(names) - begin(names); ++i) {
+        cout << names[i] << endl;
+    }
+
     return 0;
 }
