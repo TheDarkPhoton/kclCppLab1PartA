ab5ec750f5d76f6cc4b878410061e8269b83e2fe
Dovydas Rupsys <dovydas.rupsys@kcl.ac.uk>
2015-11-12 06:04:36 +0000

    Main loop setup

--- main.cpp
+++ main.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
 #include <vector>
 
+#include "Random.h"
+
 using namespace std;
 
 int main() {
-    cout << "Hello World" << endl;
+    Random random;
 
-    //copy the list of the array.
-//    string names[] = {
-//            "January", "February",
-//            "March", "April", "May",
-//            "June", "July", "August",
-//            "September", "October", "November",
-//            "December"
-//    };
+    vector<string> words = {
+        "Tree", "House", "Mask"
+    };
 
-    vector<string> names({
-         "January", "February",
-         "March", "April", "May",
-         "June", "July", "August",
-         "September", "October", "November",
-         "December"
-    });
+    int random_int = random.integral(0, (int)words.size() - 1);
+    string selected_word = words[random_int];
 
-    for (int i = 0; i < names.size(); ++i) {
-        cout << names[i] << endl;
+    string masked_word = "";
+    for (int i = 0; i < selected_word.size(); ++i) {
+        masked_word += "_";
     }
+
+    int tries = 6;
+
+    string input;
+    do {
+        cout << masked_word << endl;
+        cout << "Enter a letter" << endl;
+        getline(cin, input);
+
+        if (input != "exit"){
+
+        }
+    } while (input == "exit");
 
     return 0;
 }
