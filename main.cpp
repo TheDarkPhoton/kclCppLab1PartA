4402336ba91acc8b23850c5467ca714b7b4dd3d6
Dovydas Rupsys <dovydas.rupsys@kcl.ac.uk>
2015-11-12 06:07:23 +0000

    Reset main to Hello World example.

--- main.cpp
+++ main.cpp
@@ -1,31 +1,10 @@
 #include <iostream>
-#include <vector>
+#include "Random.h"
 
 using namespace std;
 
 int main() {
     cout << "Hello World" << endl;
-
-    //copy the list of the array.
-//    string names[] = {
-//            "January", "February",
-//            "March", "April", "May",
-//            "June", "July", "August",
-//            "September", "October", "November",
-//            "December"
-//    };
-
-    vector<string> names({
-         "January", "February",
-         "March", "April", "May",
-         "June", "July", "August",
-         "September", "October", "November",
-         "December"
-    });
-
-    for (int i = 0; i < names.size(); ++i) {
-        cout << names[i] << endl;
-    }
 
     return 0;
 }
